eede6ee12dad947c405023e5c849ddd2b1d1d326
Павел Гаджиев <gadzhievpasha@gmail.com>
2024-10-04 15:34:55 +0300

    Добавлена валидация guid

---
+++ DeveloperUtilsLibrary/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DeveloperUtilsLibrary
+{
+    public class Validator
+    {
+        /// <summary>
+        /// Проверка на верность написания GUID
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool isValidGUID(string str)
+        {
+            string strRegex = @"^[{]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[}]?$";
+            Regex re = new Regex(strRegex);
+            if (re.IsMatch(str))
+                return (true);
+            else
+                return (false);
+        }
+    }
+}
